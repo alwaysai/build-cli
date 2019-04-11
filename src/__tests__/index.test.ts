5f73bf3e346a4a59f6e082a6c280dab8f011c079
Chris Arnesen <chris@alwaysai.co>
2019-04-11 11:18:50 -0700

    Prepare for publish

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,9 @@
 import { runAndCatch } from '@alwaysai/always-cli';
 
-import { alwaysai } from '..';
+import { cli } from '..';
 
 describe('index file', () => {
   it('shows usage', async () => {
-    expect(await runAndCatch(alwaysai, '--help')).toMatch(/usage/i);
-  });
-
-  it('version returns version', async () => {
-    const semverRegex = /\..*\..*/;
-    expect(await alwaysai('version')).toMatch(semverRegex);
+    expect(await runAndCatch(cli, '--help')).toMatch(/usage/i);
   });
 });
